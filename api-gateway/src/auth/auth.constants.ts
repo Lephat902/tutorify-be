6ba72b570b426b461f273855348fe42e31159060
Lephat902 <lehoangngocphat992002@gmail.com>
2024-01-20 15:55:06 +0700

    Removed subfolders

--- api-gateway/src/auth/auth.constants.ts
+++
@@ -1,8 +0,0 @@
-export const AuthConstants = {
-  access_token: {
-    options: {
-      expiresIn: '30m',
-      issuer: 'ISSUER_NAME',
-    },
-  },
-}
--- api-gateway/src/auth/token.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const Token = createParamDecorator((_, context: ExecutionContext) => {
-  const req = context.switchToHttp().getRequest();
-
-  return req.token || null;
-});
