209e6b0eda5a47cabf69552db97a7f0a1eab793a
Lephat902 <lehoangngocphat992002@gmail.com>
2024-02-18 18:03:50 +0700

    Add support for class timeslots

---
+++ shared/enums/class/index.ts
@@ -0,0 +1,4 @@
+export * from './weekday.enum';
+export * from './tutor-position-pref.enum';
+export * from './gender-pref.enum';
+export * from './class-order-by.enum';
---
+++ shared/enums/class/weekday.enum.ts
@@ -0,0 +1,9 @@
+export enum Weekday {
+    MONDAY = 'MON',
+    TUESDAY = 'TUE',
+    WEDNESDAY = 'WED',
+    THURSDAY = 'THU',
+    FRIDAY = 'FRI',
+    SATURDAY = 'SAT',
+    SUNDAY = 'SUN'
+}
