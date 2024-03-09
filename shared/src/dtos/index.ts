90e36735f2abbf8c8ee89d236df7f7d4e145b612
Lephat902 <lehoangngocphat992002@gmail.com>
2024-03-09 09:40:59 +0700

    Add proficiencies field in tutor-query graphql, fix query error with q param in tutor-query

--- shared/src/dtos/index.ts
+++ shared/src/dtos/index.ts
@@ -2,3 +2,4 @@ export * from './pagination.dto';
 export * from './sort-dir.dto';
 export * from './file-upload-response.dto';
 export * from './tutor-apply-for-class';
+export * from './class-category';
